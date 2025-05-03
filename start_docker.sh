48c08753f3552b99972c67b454a88e7f5914c165
nialldevlin <32718927+nialldevlin@users.noreply.github.com>
2025-05-03 01:34:39 -0600

    devices

--- start_docker.sh
+++ start_docker.sh
@@ -227,6 +227,7 @@ DOCKER_RUN_FLAGS+=("--device=/dev/net/tun")
 mkdir -p /tmp/shared
 DOCKER_RUN_FLAGS+=("--volume=/tmp/shared:/tmp/shared:rw")
 DOCKER_RUN_FLAGS+=("--device=/dev/gpiomem")
+DOCKER_RUN_FLAGS+=("--device=/dev/mem")
 
 # Add Docker flag to mount /dev with correct permissions
 DOCKER_RUN_FLAGS+=("--volume=/dev:/dev:rw")
