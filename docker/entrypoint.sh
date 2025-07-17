ed67470ea02e55cee9f20d26052d6feaa020e487
Niall Devlin <niallcdevlin@gmail.com>
2025-07-17 12:26:42 -0600

    fixing BNO085

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,9 +4,5 @@ source /opt/ros/$ROS_DISTRO/setup.bash
 source $HOME/ros2_roboclaw_driver/install/setup.bash
 source $HOME/ros2_ws/install/setup.bash
 
-# Set group and permissions for GPIO devices
-chgrp gpio /dev/gpiochip* /dev/gpiomem* || true
-chmod g+rw /dev/gpiochip* /dev/gpiomem* || true
-
 # Execute the passed commandex
 exec "$@"
