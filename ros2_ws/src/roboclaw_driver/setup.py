1a47f091b2c80b9ab1a9e6f2655764a8cdab14c5
Niall Devlin <niallcdevlin@gmail.com>
2025-07-28 10:30:33 -0600

    nav 2

--- ros2_ws/src/roboclaw_driver/setup.py
+++ ros2_ws/src/roboclaw_driver/setup.py
@@ -11,6 +11,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name + '/launch', glob('launch/*.py')),
+        ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
