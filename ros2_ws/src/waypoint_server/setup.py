123fbd18c0eee5005066c2e12053e61741445237
MissionTrouble <mcgranens@gmail.com>
2025-11-12 15:06:41 -0700

    new setup.py

--- ros2_ws/src/waypoint_server/setup.py
+++ ros2_ws/src/waypoint_server/setup.py
@@ -4,19 +4,24 @@ package_name = 'waypoint_server'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.0.0',
     packages=[package_name],
-    package_dir={'': 'src'},
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/launch_waypoint_system.launch.py']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='YOUR_NAME',
-    maintainer_email='YOUR_EMAIL@example.com',
-    description='A simple ROS2 waypoint server node.',
-    license='Apache License 2.0',
-    tests_require=['pytest'],
+    maintainer='walle',
+    maintainer_email='you@example.com',
+    description='Waypoint management and following nodes',
+    license='MIT',
     entry_points={
         'console_scripts': [
-            'waypoint_server = waypoint_server.waypoint_server_node:main'
+            'waypoint_server_node = waypoint_server.waypoint_server_node:main',
+            'waypoint_follower_node = waypoint_server.waypoint_follower_node:main',
         ],
     },
 )
