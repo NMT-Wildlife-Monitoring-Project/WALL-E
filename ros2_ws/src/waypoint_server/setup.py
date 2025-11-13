0b6194777851839b809fce823239440182a7066e
MissionTrouble <mcgranens@gmail.com>
2025-11-13 15:23:13 -0700

    fix??

--- ros2_ws/src/waypoint_server/setup.py
+++ ros2_ws/src/waypoint_server/setup.py
@@ -4,22 +4,22 @@ package_name = 'waypoint_server'
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name + '/launch', ['launch/launch_waypoint_system.launch.py']),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='walle',
+    maintainer='your_name',
     maintainer_email='you@example.com',
-    description='Waypoint server and follower nodes',
-    license='MIT',
+    description='Simple ROS2 Python example',
+    license='Apache License 2.0',
     entry_points={
         'console_scripts': [
-            'waypoint_follower_node = waypoint_server.waypoint_follower_node:main',
+            'waypoint_follower_node = waypoint_server.waypoint_follower_node:main'
         ],
     },
 )
