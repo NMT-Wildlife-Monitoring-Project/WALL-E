ac732a5e199c11096940d7844195e5552db5ed15
MissionTrouble <mcgranens@gmail.com>
2025-11-14 12:11:22 -0700

    changed launch file

--- ros2_ws/src/waypoint_server/launch/launch_waypoint_system.launch.py
+++ ros2_ws/src/waypoint_server/launch/launch_waypoint_system.launch.py
@@ -5,22 +5,22 @@ from launch_ros.actions import Node
 def generate_launch_description():
     waypoint_follower = Node(
         package='waypoint_server',
-        executable='waypoint_follower_node',
+        executable='gps_waypoint_handler_node.py',
         name='waypoint_follower',
         output='screen',
     )
 
-    waypoint_interactive_markers = Node(
-        package='robot_navigation',
-        executable='waypoint_interactive_markers',
-        name='waypoint_interactive_markers',
-        output='screen',
-        parameters=[
-            {'fixed_frame': 'map'},
-        ],
-    )
+#    waypoint_interactive_markers = Node(
+#        package='robot_navigation',
+#        executable='waypoint_interactive_markers',
+#        name='waypoint_interactive_markers',
+#        output='screen',
+#        parameters=[
+#            {'fixed_frame': 'map'},
+#        ],
+#    )
 
     return LaunchDescription([
         waypoint_follower,
-        waypoint_interactive_markers,
+#        waypoint_interactive_markers,
     ])
