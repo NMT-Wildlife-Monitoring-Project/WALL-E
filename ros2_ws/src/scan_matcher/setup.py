8e9ec7bf063992b68654a67a4b9f7eb06478fb6e
Niall Devlin <niallcdevlin@gmail.com>
2025-12-03 13:07:42 -0700

    Install scan_matcher launch file

--- ros2_ws/src/scan_matcher/setup.py
+++ ros2_ws/src/scan_matcher/setup.py
@@ -11,7 +11,8 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        # Install all launch files (we name them *_launch.py)
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
