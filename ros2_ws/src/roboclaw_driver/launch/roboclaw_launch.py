5c139998c749a3f8b9dd97190d7f0ec3806983f6
MissionTrouble <mcgranens@gmail.com>
2026-03-03 15:38:21 -0700

    reduce speed

--- ros2_ws/src/roboclaw_driver/launch/roboclaw_launch.py
+++ ros2_ws/src/roboclaw_driver/launch/roboclaw_launch.py
@@ -12,7 +12,7 @@ def generate_launch_description():
         DeclareLaunchArgument('qppr', default_value='6533'),
         DeclareLaunchArgument('accel', default_value='1.5'),
         DeclareLaunchArgument('max_speed', default_value='0.4'),
-        DeclareLaunchArgument('max_speed_qpps', default_value='10560'),
+        DeclareLaunchArgument('max_speed_qpps', default_value='4224'),#previously 10560, 30v -> 4224 for about 12v
         DeclareLaunchArgument('accel_qpps', default_value='-1'),
         DeclareLaunchArgument('wheel_separation', default_value='0.39'),
         DeclareLaunchArgument('wheel_diameter', default_value='0.095'),
