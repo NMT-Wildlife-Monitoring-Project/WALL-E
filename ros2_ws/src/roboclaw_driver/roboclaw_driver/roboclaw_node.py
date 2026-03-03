63185426a54d3d5150cb6b73ee9355195394b387
Emiliano Mota <emilianomota938@gmail.com>
2026-03-02 20:17:26 -0700

    Reduce motor speed by 45% due to higher battery voltage
    max_speed 1.0 -> 0.55 m/s
    max_speed default 1.0 -> 0.55 m/s
    scale_linear 1.0 -> 0.55 m/s, scale_angular 3.0 -> 1.65 rad/s

--- ros2_ws/src/roboclaw_driver/roboclaw_driver/roboclaw_node.py
+++ ros2_ws/src/roboclaw_driver/roboclaw_driver/roboclaw_node.py
@@ -48,11 +48,11 @@ class RoboclawNode(Node):
         self.declare_parameter('address', 128)
         self.declare_parameter('qppr', 6533)  # Quadrature pulses per revolution
         self.declare_parameter('accel', 1.5)    # m/s^2
-        self.declare_parameter('max_speed', 1.0) # m/s
+        self.declare_parameter('max_speed', 0.55) # m/s
         self.declare_parameter('max_speed_qpps', 10560)  # Max speed in quadrature pulses per second, -1 means use max_speed
         self.declare_parameter('accel_qpps', -1)      # Max accel in quadrature pulses per second^2, -1 means use accel
         self.declare_parameter('wheel_separation', 0.24) # meters
-        self.declare_parameter('wheel_diameter', 0.105)    # meters
+        self.declare_parameter('wheel_diameter', 0.093)    # meters
         self.declare_parameter('m1_reverse', True)  # Reverse motor 1 direction
         self.declare_parameter('m2_reverse', False)  # Reverse motor 2 direction
         self.declare_parameter('odom_publish_rate', 20)  # Hz
