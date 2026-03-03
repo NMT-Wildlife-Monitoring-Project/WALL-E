aa6b6a15ea3e78932536485fce9f84557c84e539
Emiliano Mota <emilianomota938@gmail.com>
2026-03-03 14:59:13 -0700

    Increase max motor speed from 0.1 to 0.2

--- ros2_ws/src/roboclaw_driver/roboclaw_driver/roboclaw_node.py
+++ ros2_ws/src/roboclaw_driver/roboclaw_driver/roboclaw_node.py
@@ -48,7 +48,7 @@ class RoboclawNode(Node):
         self.declare_parameter('address', 128)
         self.declare_parameter('qppr', 6533)  # Quadrature pulses per revolution
         self.declare_parameter('accel', 1.5)    # m/s^2
-        self.declare_parameter('max_speed', 0.1) # m/s
+        self.declare_parameter('max_speed', 0.2) # m/s
         self.declare_parameter('max_speed_qpps', 10560)  # Max speed in quadrature pulses per second, -1 means use max_speed
         self.declare_parameter('accel_qpps', -1)      # Max accel in quadrature pulses per second^2, -1 means use accel
         self.declare_parameter('wheel_separation', 0.24) # meters
